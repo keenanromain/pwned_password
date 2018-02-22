a177e465a738f4ba606d7acc91deefc28b8973ce
Keenan Romain <keenan.romain@gmail.com>
2018-02-22 01:14:27 -0500

    first commit

---
+++ pwned_password.rb
@@ -0,0 +1,28 @@
+require 'digest'
+require 'io/console'
+require 'net/http'
+require 'uri'
+
+print "Enter password to be searched in the PwnedPasswords.com database: "
+password = STDIN.noecho(&:gets).chomp
+hashedpw = (Digest::SHA1.hexdigest password).upcase
+
+uri = URI.parse('https://api.pwnedpasswords.com/range/'+hashedpw[0...5])
+response = Net::HTTP.get_response(uri)
+
+pwned = false
+if response.kind_of? Net::HTTPSuccess
+    response.body.split("\n").each do |item|
+        if item.include? hashedpw[5..-1]
+            pwned = true
+            puts "\nYour password is not secure! Occurences found: "+item.split(":")[1]
+            break
+        end
+    end
+    if not pwned
+        puts "\nYour password has not been found in the PwnedPasswords.com database"
+    end
+else
+    puts "\nOops! Error querying API."
+end
+
